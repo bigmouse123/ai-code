9e67b348a687e5adf30f40d6fa69b464860b9880
lijiankun <lijiankun@jbinfo.cn>
2026-02-23 01:46:59 +0800

    refactor: 模板模式优化文件保存器

---
+++ src/main/java/com/jiankun/aicode/core/saver/HtmlCodeFileSaverTemplate.java
@@ -0,0 +1,37 @@
+package com.jiankun.aicode.core.saver;
+
+import cn.hutool.core.util.StrUtil;
+import com.jiankun.aicode.exception.BusinessException;
+import com.jiankun.aicode.exception.ErrorCode;
+import com.jiankun.aicode.model.HtmlCodeResult;
+import com.jiankun.aicode.model.enums.CodeGenTypeEnum;
+
+/**
+ * HTML代码文件保存器
+ *
+ * @author lijiankun
+ * @since 2026/2/23
+ */
+public class HtmlCodeFileSaverTemplate extends CodeFileSaverTemplate<HtmlCodeResult> {
+
+    @Override
+    protected CodeGenTypeEnum getCodeType() {
+        return CodeGenTypeEnum.HTML;
+    }
+
+    @Override
+    protected void saveFiles(HtmlCodeResult result, String baseDirPath) {
+        // 保存 HTML 文件
+        writeToFile(baseDirPath, "index.html", result.getHtmlCode());
+    }
+
+    @Override
+    protected void validateInput(HtmlCodeResult result) {
+        super.validateInput(result);
+        // HTML 代码不能为空
+        if (StrUtil.isBlank(result.getHtmlCode())) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "HTML代码内容不能为空");
+        }
+    }
+}
+
---
+++ src/main/java/com/jiankun/aicode/core/saver/MultiFileCodeFileSaverTemplate.java
@@ -0,0 +1,40 @@
+package com.jiankun.aicode.core.saver;
+
+import cn.hutool.core.util.StrUtil;
+import com.jiankun.aicode.exception.BusinessException;
+import com.jiankun.aicode.exception.ErrorCode;
+import com.jiankun.aicode.model.MultiFileCodeResult;
+import com.jiankun.aicode.model.enums.CodeGenTypeEnum;
+
+/**
+ * 多文件代码保存器
+ *
+ * @author lijiankun
+ * @since 2026/2/23
+ */
+public class MultiFileCodeFileSaverTemplate extends CodeFileSaverTemplate<MultiFileCodeResult> {
+
+    @Override
+    public CodeGenTypeEnum getCodeType() {
+        return CodeGenTypeEnum.MULTI_FILE;
+    }
+
+    @Override
+    protected void saveFiles(MultiFileCodeResult result, String baseDirPath) {
+        // 保存 HTML 文件
+        writeToFile(baseDirPath, "index.html", result.getHtmlCode());
+        // 保存 CSS 文件
+        writeToFile(baseDirPath, "style.css", result.getCssCode());
+        // 保存 JavaScript 文件
+        writeToFile(baseDirPath, "script.js", result.getJsCode());
+    }
+
+    @Override
+    protected void validateInput(MultiFileCodeResult result) {
+        super.validateInput(result);
+        // 至少要有 HTML 代码，CSS 和 JS 可以为空
+        if (StrUtil.isBlank(result.getHtmlCode())) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "HTML代码内容不能为空");
+        }
+    }
+}
